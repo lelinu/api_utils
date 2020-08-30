b9c5765828f2da9248eda4cce6ca18bda1e1b987
lelinu <emanuel.mercieca@knowmenow.com+2-at-159440491394>
2020-08-30 18:38:10 +0200

    Added gorm pagination

---
+++ utils/db_utils/gorm_pagination.go
@@ -0,0 +1,67 @@
+package db_utils
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+//Pagination struct
+type Pagination struct {
+	DB      *gorm.DB
+	OrderBy []string
+	Page    int64
+	PerPage int64
+}
+
+//PaginationResult struct
+type PaginationResult struct {
+	TotalRecords int         `json:"totalRecords"`
+	Records      interface{} `json:"records"`
+	CurrentPage  int64       `json:"currentPage"`
+	TotalPages   int64       `json:"totalPages"`
+}
+
+//Paginate function
+func (p *Pagination) Paginate(dataSource interface{}) *PaginationResult {
+	db := p.DB
+
+	if len(p.OrderBy) > 0 {
+		for _, o := range p.OrderBy {
+			db = db.Order(o)
+		}
+	}
+
+	done := make(chan bool, 1)
+	var output PaginationResult
+	var count int
+	var offset int64
+
+	go countRecords(db, dataSource, done, &count)
+
+	if p.Page == 1 {
+		offset = 0
+	} else {
+		tmpPage := p.Page
+		tmpPerPage := p.PerPage
+		offset = (tmpPage - 1) * tmpPerPage
+	}
+
+	db.Limit(p.PerPage).Offset(offset).Find(dataSource)
+	<-done
+
+	output.TotalRecords = count
+	output.Records = dataSource
+	output.CurrentPage = p.Page
+	output.TotalPages = getTotalPages(p.PerPage, count)
+
+	return &output
+}
+
+func countRecords(db *gorm.DB, countDataSource interface{}, done chan bool, count *int) {
+	db.Model(countDataSource).Count(count)
+	done <- true
+}
+
+func getTotalPages(perPage int64, totalRecords int) int64 {
+	totalPages := float64(totalRecords) / float64(perPage)
+	return int64(totalPages)
+}
